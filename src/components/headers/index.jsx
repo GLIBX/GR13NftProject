f5a2a2c38ab76eea31713f72f2c15a77ce59519c
Pycomet <alfredemmanuelinyang@gmail.com>
2022-05-03 16:40:12 +0100

    update

--- src/components/headers/index.jsx
+++ src/components/headers/index.jsx
@@ -56,96 +56,96 @@ const NavSection = () => {
     const chainData = [...Chains]
 
     return (
-    <Box textAlign="center" fontSize="xl" minH="10vh" minW="100vw" p={20} pt={10}>
-        <Stack direction="row" spacing={10}>
-            <Heading fontSize="3xl" pt={2} as={Link} to="/">MintWave.</Heading>
+        <Box textAlign="center" fontSize="xl" minH="10vh" minW="100vw" p={20} pt={10}>
+            <Stack direction="row" spacing={10}>
+                <Heading fontSize="3xl" pt={2} as={Link} to="/">MintWave.</Heading>
 
-            <SearchBox placeholder="Search items, collections and accounts.."/>
+                <SearchBox placeholder="Search items, collections and accounts.."/>
 
-            <Spacer />
-            <Menu>
-                <MenuButton as={Button} variant={'outline'} rightIcon={<ChevronDownIcon />}>
-                    Explore
-                </MenuButton>
-                <MenuList p={2}>
-                    <MenuItem as={Button} variant={'ghost'} pl={10} justifyContent={'left'}>
-                        <Link to="/explore-all">All Collections</Link>
-                    </MenuItem>
-                    {chainData.map((each, id) => {
-                        return (<MenuItem as={Button} variant={'ghost'} onClick={() => handleExploreClick(each.id)} justifyContent={'left'} key={id}><Image src={each.logo_url} w={'1em'} mx={2} /> {each.label} </MenuItem>)
-                    })}
-                </MenuList>
-            </Menu>
+                <Spacer />
+                <Menu>
+                    <MenuButton as={Button} variant={'outline'} rightIcon={<ChevronDownIcon />}>
+                        Explore
+                    </MenuButton>
+                    <MenuList p={2}>
+                        <MenuItem as={Button} variant={'ghost'} pl={10} justifyContent={'left'}>
+                            <Link to="/explore-all">All Collections</Link>
+                        </MenuItem>
+                        {chainData.map((each, id) => {
+                            return (<MenuItem as={Button} variant={'ghost'} onClick={() => handleExploreClick(each.id)} justifyContent={'left'} key={id}><Image src={each.logo_url} w={'1em'} mx={2} /> {each.label} </MenuItem>)
+                        })}
+                    </MenuList>
+                </Menu>
 
-            <Menu>
-                <MenuButton as={Button} variant={'outline'} rightIcon={<ChevronDownIcon />}>
-                    Stats
-                </MenuButton>
-                <MenuList p={2}>
-                    <MenuItem as={Button} variant={'ghost'} justifyContent={'left'}>Rankings</MenuItem>
-                    <MenuItem as={Button} variant={'ghost'} justifyContent={'left'}>Activity</MenuItem>
-                </MenuList>
-            </Menu>
-            <Button variant={'outline'} as={Link} to="/">Resources</Button>
-            <Button variant={'outline'} as={Link} to="/">Create</Button>
+                <Menu>
+                    <MenuButton as={Button} variant={'outline'} rightIcon={<ChevronDownIcon />}>
+                        Stats
+                    </MenuButton>
+                    <MenuList p={2}>
+                        <MenuItem as={Button} variant={'ghost'} justifyContent={'left'}>Rankings</MenuItem>
+                        <MenuItem as={Button} variant={'ghost'} justifyContent={'left'}>Activity</MenuItem>
+                    </MenuList>
+                </Menu>
+                <Button variant={'outline'} as={Link} to="/">Resources</Button>
+                <Button variant={'outline'} as={Link} to="/">Create</Button>
 
 
-            {!active ? (
-                <>
-                    <Avatar src='https://bit.ly/broken-link' size={'md'} />
-                    <Button onClick={onOpen} bg={'#575FCC'} color={'#fff'} px={10} py={5}>Connect Wallet</Button>
-                    <SelectWalletModal isOpen={isOpen} closeModal={onClose} />
-                </>
-            ) : (
-                <>
-                    <Menu>
-                        <MenuButton as={Avatar}  src={AvatarIcon} size={'md'} />
-                        <MenuList p={2}>
-                            <MenuItem as={Button} variant={'ghost'} justifyContent={'left'}>Profile</MenuItem>
-                            <MenuItem as={Button} variant={'ghost'} justifyContent={'left'}>Favorites</MenuItem>
-                            <MenuItem as={Button} variant={'ghost'} justifyContent={'left'}>Watchlist</MenuItem>
-                            <MenuItem as={Button} variant={'ghost'} justifyContent={'left'}>My Collections</MenuItem>
-                            <MenuItem as={Button} variant={'ghost'} justifyContent={'left'}>Settings</MenuItem>
-                            <MenuItem as={Button} variant={'ghost'} justifyContent={'left'} onClick={disconnect}>Log Out</MenuItem>
-                        </MenuList>
-                    </Menu>
-                    <Avatar as={BiWallet} bg={'none'} onClick={onOpen} ref={walletRef}/>
-                    {/* <Button onClick={disconnect} bg={'#575FCC'} color={'#fff'}>{truncateText(account)}</Button> */}
-                    <Drawer
-                        isOpen={isOpen}
-                        placement='right'
-                        onClose={onClose}
-                        finalFocusRef={walletRef}
-                        paddingTop='20vh'
-                        sizer='md'
-                    >
-                        <DrawerOverlay />
-                        <DrawerContent>
-                        <DrawerCloseButton />
-                        <DrawerHeader>My Wallet</DrawerHeader>
-                        <hr></hr>
+                {!active ? (
+                    <>
+                        <Avatar src='https://bit.ly/broken-link' size={'md'} />
+                        <Button onClick={onOpen} bg={'#575FCC'} color={'#fff'} px={10} py={5}>Connect Wallet</Button>
+                        <SelectWalletModal isOpen={isOpen} closeModal={onClose} />
+                    </>
+                ) : (
+                    <>
+                        <Menu>
+                            <MenuButton as={Avatar}  src={AvatarIcon} size={'md'} />
+                            <MenuList p={2}>
+                                <MenuItem as={Button} variant={'ghost'} justifyContent={'left'}>Profile</MenuItem>
+                                <MenuItem as={Button} variant={'ghost'} justifyContent={'left'}>Favorites</MenuItem>
+                                <MenuItem as={Button} variant={'ghost'} justifyContent={'left'}>Watchlist</MenuItem>
+                                <MenuItem as={Button} variant={'ghost'} justifyContent={'left'}>My Collections</MenuItem>
+                                <MenuItem as={Button} variant={'ghost'} justifyContent={'left'}>Settings</MenuItem>
+                                <MenuItem as={Button} variant={'ghost'} justifyContent={'left'} onClick={disconnect}>Log Out</MenuItem>
+                            </MenuList>
+                        </Menu>
+                        <Avatar as={BiWallet} bg={'none'} onClick={onOpen} ref={walletRef}/>
+                        {/* <Button onClick={disconnect} bg={'#575FCC'} color={'#fff'}>{truncateText(account)}</Button> */}
+                        <Drawer
+                            isOpen={isOpen}
+                            placement='right'
+                            onClose={onClose}
+                            finalFocusRef={walletRef}
+                            paddingTop='20vh'
+                            sizer='md'
+                        >
+                            <DrawerOverlay />
+                            <DrawerContent>
+                            <DrawerCloseButton />
+                            <DrawerHeader>My Wallet</DrawerHeader>
+                            <hr></hr>
 
-                        <br></br>
-                        <br></br>
-
-                        <DrawerBody>
-                            <b>Address:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{truncateText(account)}</b>
                             <br></br>
-                            <b>Balance:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$0.0</b>
-                        </DrawerBody>
+                            <br></br>
 
-                        <DrawerFooter>
-                            <Button bg={'#575FCC'} color="#fff">Update Balance</Button>
-                        </DrawerFooter>
-                        </DrawerContent>
-                    </Drawer>
-                </>
-            )}
-        </Stack>
-        <Stack direction="row" borderBottom={'1px solid'} pt={'1em'} spacing={10} opacity="0.1" />
+                            <DrawerBody>
+                                <b>Address:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{truncateText(account)}</b>
+                                <br></br>
+                                <b>Balance:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$0.0</b>
+                            </DrawerBody>
 
-        
-    </Box>
+                            <DrawerFooter>
+                                <Button bg={'#575FCC'} color="#fff">Update Balance</Button>
+                            </DrawerFooter>
+                            </DrawerContent>
+                        </Drawer>
+                    </>
+                )}
+            </Stack>
+            <Stack direction="row" borderBottom={'1px solid'} pt={'1em'} spacing={10} opacity="0.1" />
+
+            
+        </Box>
     )
 };
 
